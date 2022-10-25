52f590a8e49fb8cfad0b478161c470c37ea109fe
Hikanon <roma.makhanov@mail.ru>
2022-10-25 23:07:40 +0300

    He

--- src/main/java/com/tgbot/springtelegrambot/config/BotConfig.java
+++ src/main/java/com/tgbot/springtelegrambot/config/BotConfig.java
@@ -1,2 +1,24 @@
-package com.tgbot.springtelegrambot.config;public class BotConfig {
+package com.tgbot.springtelegrambot.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("application.properties")
+public class BotConfig {
+
+    @Value("${bot.name}")
+    String botName;
+
+    @Value("${bot.token}")
+    String botToken;
+
+    public String getBotName() {
+        return botName;
+    }
+
+    public String getBotToken() {
+        return botToken;
+    }
 }
--- src/main/java/com/tgbot/springtelegrambot/service/TelegramBot.java
+++ src/main/java/com/tgbot/springtelegrambot/service/TelegramBot.java
@@ -1,2 +1,41 @@
-package com.tgbot.springtelegrambot.service;public class TelegramBot {
+package com.tgbot.springtelegrambot.service;
+
+import com.tgbot.springtelegrambot.config.BotConfig;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBot extends TelegramLongPollingCommandBot {
+    private final BotConfig config;
+
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getBotToken();
+    }
+
+    private void sendMessage(long chatId, String text){
+        SendMessage message = new SendMessage(String.valueOf(chatId), text);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void processNonCommandUpdate(Update update) {
+        sendMessage(update.getMessage().getChatId(), "Ты куда жмал?");
+    }
 }
